aa196e32871ad7cf3a90bc5e17f377bbec42b1dc
Yidan Zhang <zyd991123@gmail.com>
2024-03-06 22:21:33 +0700

    assignment01

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Shoe:
         self.price = price
 
     def display(self):
-        return f"{self.brand} {self.model} - Size: {self.size}, Price: ฿{self.price}"
+        return f"{self.brand} {self.model} - Size: UK{self.size}, Price: ฿{self.price}"
 
 
 class TennisShoe(Shoe):
@@ -15,13 +15,13 @@ class TennisShoe(Shoe):
         self.court = court
 
     def display(self):
-        return f"{self.brand} {self.model} - Size: {self.size}, Court: {self.court}, Price: ${self.price}"
+        return f"{self.brand} {self.model} - Size: UK{self.size}, Court: {self.court}, Price: ฿{self.price}"
 
 
 if __name__ == "__main__":
     # Example usage
     shoe1 = Shoe("Adidas", "SAMBA OG SHOES", 4.5, 3800)
-    print(shoe1.display())  # Output: Nike Air Max - Size: 10, Price: $120
+    print(shoe1.display())
 
     ADIFOM_shoe1 = TennisShoe("Adidas", "ADIFOM STAN SMITH MULE SHOES", 7, "Hard", 2500)
-    print(ADIFOM_shoe1.display())  # Output: Adidas Barricade - Size: 9.5, Court: Hard, Price: $150
+    print(ADIFOM_shoe1.display())
