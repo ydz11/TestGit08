abec1a28ba82da78809cb7101235f5a78ab68690
Yidan Zhang <zyd991123@gmail.com>
2024-03-06 22:25:34 +0700

    assignment01

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Shoe:
         return f"{self.brand} {self.model} - Size: UK{self.size}, Price: ฿{self.price}"
 
 
-class TennisShoe(Shoe):
+class ADIFOM_shoe(Shoe):
     def __init__(self, brand, model, size, court, price):
         super().__init__(brand, model, size, price)
         self.court = court
@@ -23,5 +23,5 @@ if __name__ == "__main__":
     shoe1 = Shoe("Adidas", "SAMBA OG SHOES", 4.5, 3800)
     print(shoe1.display())
 
-    ADIFOM_shoe1 = TennisShoe("Adidas", "ADIFOM STAN SMITH MULE SHOES", 7, "Hard", 2500)
+    ADIFOM_shoe1 = ADIFOM_shoe("Adidas", "ADIFOM STAN SMITH MULE SHOES", 7, "Hard", 2500)
     print(ADIFOM_shoe1.display())
